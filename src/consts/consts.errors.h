34bb9ff7c496b8690ae6e622128dcfada277b645
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-09 00:28:26 -0300

    att

--- src/consts/consts.errors.h
+++ src/consts/consts.errors.h
@@ -6,7 +6,7 @@
 
 
 #define FILE_NOT_PROVIDED_ERROR "file %s not provides  end scope\n"
-#define SRC_FLAG_NOT_PROVIDED "src flag not provided"
-#define TAG_FLAG_NOT_PROVIDED "tag flag not provided"
-#define SRC_ITS_NOT_A_DIR "src flag its not a dir"
-#define AT_LEAST_ONE_TAG_ERROR "at least one tag must be provided"
+#define SRC_FLAG_NOT_PROVIDED "src flag not provided\n"
+#define TAG_FLAG_NOT_PROVIDED "tag flag not provided\n"
+#define SRC_ITS_NOT_A_DIR "src flag its not a dir\n"
+#define AT_LEAST_ONE_TAG_ERROR "at least one tag must be provided\n"
