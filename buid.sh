bd7588c6725793ddb9a09e00d76d6e22b2635ee9
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-09 02:45:12 -0300

    att

--- buid.sh
+++ buid.sh
@@ -2,5 +2,5 @@
 #-tags dependencies consts types globals func_declaration func_definition
 
 gcc src/main.c -o silverchain.out
-valgrind ./silverchain.out -src src \
+./silverchain.out -src src \
 -tags dependencies consts types globals func_declaration func_definition
---
+++ build_plataforms.sh
@@ -0,0 +1,3 @@
+
+ gcc -Wall -pedantic  -o silverchain.out src/main.c 
+ x86_64-w64-mingw32-gcc -Wall -pedantic  -o silverchain.exe src/main.c 
