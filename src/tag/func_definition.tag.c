60901158ece76a7e7ea85c89ad6851fa5ce2e229
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-08 23:10:56 -0300

    att

--- src/tag/func_definition.tag.c
+++ src/tag/func_definition.tag.c
@@ -44,6 +44,40 @@ void Tag_create_module_file(
     dtw.write_string_file_content(final_text_path->rendered_text,final_text->rendered_text);
     stack.free(final_text);
 }
+void replace_import_file(const char *current_file_path,const char *module_path){
+    int end_scope_size = (int)strlen(SILVER_CHAIN_END_SCOPE);
+    CTextStack *relative_path = make_relative_path(current_file_path,module_path);
+    CTextStack *text_to_insert = stack.newStack_string(SILVER_CHAIN_START_SCOPE);
+    stack.format(text_to_insert,"%s",MANAGED_SYSTEM);
+    stack.format(text_to_insert,"#include \"%t\"\n",relative_path);
+    stack.text(text_to_insert,SILVER_CHAIN_END_SCOPE);
+    stack.free(relative_path);
+
+    char *file_content = dtw.load_string_file_content(current_file_path);
+    CTextStack *file_content_stack = stack.newStack_string(file_content);
+    int start_scope_index = stack.index_of(file_content_stack,SILVER_CHAIN_START_SCOPE);
+    if(start_scope_index == -1){
+        //means its not implemented
+        stack.self_insert_at(file_content_stack,0,text_to_insert->rendered_text);
+        dtw.write_string_file_content(current_file_path,file_content_stack->rendered_text);
+        stack.free(file_content_stack);
+        stack.free(text_to_insert);
+        return;;
+    }
+
+    int end_scope_index = stack.index_of(file_content_stack,SILVER_CHAIN_END_SCOPE);
+    if(end_scope_index == -1){
+        printf(FILE_NOT_PROVIDED_ERROR,current_file_path);
+        return;;
+    }
+    //replace the content
+    stack.self_pop(file_content_stack,start_scope_index,end_scope_index+end_scope_size-1);
+    stack.self_insert_at(file_content_stack,start_scope_index,text_to_insert->rendered_text);
+    dtw.write_string_file_content(current_file_path,file_content_stack->rendered_text);
+    stack.free(file_content_stack);
+    stack.free(text_to_insert);
+}
+
 
 void Tag_replace_import_in_files(
     Tag *self,
@@ -54,41 +88,10 @@ void Tag_replace_import_in_files(
         return;
     }
     CTextStack *module_path = stack.newStack_string_empty();
-    stack.format(module_path,"%s/imports.%s.h",module_dir,prev);
-    int end_scope_size = (int)strlen(SILVER_CHAIN_END_SCOPE);
+    stack.format(module_path,"%s/%s.%s.h",module_dir,IMPORT_NAME,prev);
     for(int i = 0; i < self->itens->size;i++){
-
         char *current_file_path = self->itens->strings[i];
-       CTextStack *relative_path = make_relative_path(current_file_path,module_path->rendered_text);
-        CTextStack *text_to_insert = stack.newStack_string(SILVER_CHAIN_START_SCOPE);
-        stack.format(text_to_insert,"%s",MANAGED_SYSTEM);
-        stack.format(text_to_insert,"#include \"%t\"\n",relative_path);
-        stack.text(text_to_insert,SILVER_CHAIN_END_SCOPE);
-        stack.free(relative_path);
-
-        char *file_content = dtw.load_string_file_content(current_file_path);
-        CTextStack *file_content_stack = stack.newStack_string(file_content);
-        int start_scope_index = stack.index_of(file_content_stack,SILVER_CHAIN_START_SCOPE);
-        if(start_scope_index == -1){
-            //means its not implemented
-            stack.self_insert_at(file_content_stack,0,text_to_insert->rendered_text);
-            dtw.write_string_file_content(current_file_path,file_content_stack->rendered_text);
-            stack.free(file_content_stack);
-            stack.free(text_to_insert);
-            continue;
-        }
-
-        int end_scope_index = stack.index_of(file_content_stack,SILVER_CHAIN_END_SCOPE);
-        if(end_scope_index == -1){
-            printf(FILE_NOT_PROVIDED_ERROR,current_file_path);
-           continue;
-        }
-        //replace the content
-        stack.self_pop(file_content_stack,start_scope_index,end_scope_index+end_scope_size-1);
-        stack.self_insert_at(file_content_stack,start_scope_index,text_to_insert->rendered_text);
-        dtw.write_string_file_content(current_file_path,file_content_stack->rendered_text);
-        stack.free(file_content_stack);
-        stack.free(text_to_insert);
+        replace_import_file(current_file_path,module_path->rendered_text);
     }
     stack.free(module_path);
 }
