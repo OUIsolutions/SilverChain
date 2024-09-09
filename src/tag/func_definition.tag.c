2206598827716c82c9e9be40a169d654916cb238
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-08 22:27:00 -0300

    att

--- src/tag/func_definition.tag.c
+++ src/tag/func_definition.tag.c
@@ -4,7 +4,6 @@
 #include "../imports/imports.func_declaration.h"
 //silver_chain_scope_end
 
-
 Tag *newTag(const char *name,int priority){
     Tag *self = (Tag*)malloc(sizeof(Tag));
     *self = (Tag){0};
@@ -27,7 +26,7 @@ void Tag_create_module_file(
     CTextStack *final_text = stack.newStack_string_empty();
 
     if(prev_module != NULL){
-            stack.format(final_text,"#include \"imports.%s.h\"\n",prev_module);
+            stack.format(final_text,"#include \"%s.%s.h\"\n",IMPORT_NAME, prev_module);
     }
 
 
@@ -56,19 +55,20 @@ void Tag_replace_import_in_files(
     }
     CTextStack *module_path = stack.newStack_string_empty();
     stack.format(module_path,"%s/imports.%s.h",module_dir,prev);
+    int end_scope_size = (int)strlen(SILVER_CHAIN_END_SCOPE);
     for(int i = 0; i < self->itens->size;i++){
 
         char *current_file_path = self->itens->strings[i];
        CTextStack *relative_path = make_relative_path(current_file_path,module_path->rendered_text);
-        CTextStack *text_to_insert = stack.newStack_string("\n//silver_chain_scope_start\n");
-        stack.text(text_to_insert,"//mannaged by silver chain\n");
+        CTextStack *text_to_insert = stack.newStack_string(SILVER_CHAIN_START_SCOPE);
+        stack.format(text_to_insert,"%s",MANAGED_SYSTEM);
         stack.format(text_to_insert,"#include \"%t\"\n",relative_path);
-        stack.text(text_to_insert,"//silver_chain_scope_end\n");
+        stack.text(text_to_insert,SILVER_CHAIN_END_SCOPE);
         stack.free(relative_path);
 
         char *file_content = dtw.load_string_file_content(current_file_path);
         CTextStack *file_content_stack = stack.newStack_string(file_content);
-        int start_scope_index = stack.index_of(file_content_stack,"\n//silver_chain_scope_start\n");
+        int start_scope_index = stack.index_of(file_content_stack,SILVER_CHAIN_START_SCOPE);
         if(start_scope_index == -1){
             //means its not implemented
             stack.self_insert_at(file_content_stack,0,text_to_insert->rendered_text);
@@ -78,13 +78,13 @@ void Tag_replace_import_in_files(
             continue;
         }
 
-        int end_scope_index = stack.index_of(file_content_stack,"//silver_chain_scope_end\n");
+        int end_scope_index = stack.index_of(file_content_stack,SILVER_CHAIN_END_SCOPE);
         if(end_scope_index == -1){
-            printf(" file %s not provides  end scope\n",current_file_path);
+            printf(FILE_NOT_PROVIDED_ERROR,current_file_path);
             exit(1);
         }
         //replace the content
-        stack.self_pop(file_content_stack,start_scope_index,end_scope_index+strlen("//silver_chain_scope_end\n")-1);
+        stack.self_pop(file_content_stack,start_scope_index,end_scope_index+end_scope_size-1);
         stack.self_insert_at(file_content_stack,start_scope_index,text_to_insert->rendered_text);
         dtw.write_string_file_content(current_file_path,file_content_stack->rendered_text);
         stack.free(file_content_stack);
@@ -102,7 +102,7 @@ void Tag_implement(
 ){
 
     CTextStack *import_module_file_path = stack.newStack_string_empty();
-    stack.format(import_module_file_path,"%s/imports.%s.h",module_dir,self->name);
+    stack.format(import_module_file_path,"%s/%s.%s.h",module_dir,IMPORT_NAME,self->name);
 
     Tag_create_module_file(self,import_module_file_path,prev,project_short_cut);
     Tag_replace_import_in_files(self,module_dir,prev);
