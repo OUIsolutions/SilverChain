fe5d6bce8fe56bb5aa436e924af977d4d8c43203
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-10 12:29:44 -0300

    att

--- build.sh
+++
@@ -1,6 +0,0 @@
-#./silverchain.out -src src \
-#-tags dependencies consts types globals func_declaration func_definition
-
-gcc src/main.c -o silverchain.out
-./silverchain.out -src src \
--tags dependencies consts types globals func_declaration func_definition
--- src/imports/imports.dependencies.h
+++ src/imports/imports.dependencies.h
@@ -1,6 +1,6 @@
 #ifndef silverchain_dependencies
 #define silverchain_dependencies
-#include "../dependencies/dependencies.doTheWorld.h"
 #include "../dependencies/dependencies.CTextEngine.h"
 #include "../dependencies/dependencies.UniversalGarbage.h"
+#include "../dependencies/dependencies.doTheWorld.h"
 #endif
