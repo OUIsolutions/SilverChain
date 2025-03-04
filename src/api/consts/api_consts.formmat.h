89c871a7b145026064486e484c84bed3db14ebed
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-04 18:18:35 -0300

    added manage flag

--- src/api/consts/api_consts.formmat.h
+++ src/api/consts/api_consts.formmat.h
@@ -6,7 +6,7 @@
 
 
 const char * IMPORT_NAME = "imports";
-const char *  MANAGED_SYSTEM =  "//mannaged by silver chain\n";
+const char *  MANAGED_SYSTEM =  "//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain\n";
 const char *  SILVER_CHAIN_START_SCOPE = "\n//silver_chain_scope_start\n";
 const char *  SILVER_CHAIN_END_SCOPE = "//silver_chain_scope_end\n";
 const char *  PROJECT_SHORT_CUT_DEFAULT = "silverchain";
