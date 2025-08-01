51a4b983ca051cf3c48c9191bbff5affcdc1fed0
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-01 12:21:09 -0300

    att

--- src/api/error/api_type.error.h
+++ src/api/error/api_type.error.h
@@ -1,7 +1,5 @@
 //silver_chain_scope_start
-//DONT MODIFY THIS COMMENT
-//this import is computationally generated
-//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
 #include "../../imports/imports.api_consts.h"
 //silver_chain_scope_end
 
--- src/api/remover/api_define.remover.c
+++ src/api/remover/api_define.remover.c
@@ -1,7 +1,5 @@
 //silver_chain_scope_start
-//DONT MODIFY THIS COMMENT
-//this import is computationally generated
-//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
 #include "../../imports/imports.api_declare.h"
 //silver_chain_scope_end
 
@@ -9,8 +7,23 @@
 
 void SilverCHain_remove_start_end_from_folder(const char *src){
     bool CONCAT_PATH = true;
-    DtwStringArray *files = dtw_list_files_recursively(src,CONCAT_PATH);
-        int end_scope_size = (int)strlen(SILVER_CHAIN_END_SCOPE);
+    DtwStringArray *files = NULL;
+    
+    // Check if src is a file or directory
+    if(dtw_entity_type(src) == DTW_FILE_TYPE){
+        // If it's a file, create a string array with just this file
+        files = newDtwStringArray();
+        DtwStringArray_append(files, src);
+    } else {
+        // If it's a directory, list files recursively
+        files = dtw_list_files_recursively(src, CONCAT_PATH);
+    }
+    
+    if(files == NULL){
+        return;
+    }
+    
+    int end_scope_size = (int)strlen(SILVER_CHAIN_END_SCOPE);
 
     for(int i = 0; i < files->size; i++){
         char *content = dtw_load_string_file_content(files->strings[i]);
@@ -18,26 +31,34 @@ void SilverCHain_remove_start_end_from_folder(const char *src){
             continue;
         }
 
-        CTextStack *formmated = newCTextStack_string(content);
-        int start_scope_index = CTextStack_index_of(formmated,SILVER_CHAIN_START_SCOPE);
-        if(start_scope_index == SILVER_CHAIN_NOT_FOUND){
-            CTextStack_free(formmated);
-            free(content);
-            continue;
+        CTextStack *formatted = newCTextStack_string(content);
+        bool has_changes = true;
+        
+        // Keep removing SilverChain scopes until no more are found
+        while(has_changes){
+            has_changes = false;
+            
+            int start_scope_index = CTextStack_index_of(formatted, SILVER_CHAIN_START_SCOPE);
+            if(start_scope_index == SILVER_CHAIN_NOT_FOUND){
+                break;
+            }
+
+            int end_scope_index = CTextStack_index_of(formatted, SILVER_CHAIN_END_SCOPE);
+            if(end_scope_index == -1){
+                break;
+            }
+
+            // Remove the scope content
+            CTextStack_self_pop(formatted, start_scope_index, end_scope_index + end_scope_size);
+            has_changes = true;
         }
+        
+        // Write the cleaned content back to file
+        dtw_write_string_file_content(files->strings[i], formatted->rendered_text);
 
-        int end_scope_index = CTextStack_index_of(formmated,SILVER_CHAIN_END_SCOPE);
-        if(end_scope_index == -1){
-            CTextStack_free(formmated);
-            free(content);
-            continue;
-        }
-
-        CTextStack_self_pop(formmated,start_scope_index,end_scope_index+end_scope_size);
-
-        dtw_write_string_file_content(files->strings[i],formmated->rendered_text);
-
-        CTextStack_free(formmated);
+        CTextStack_free(formatted);
         free(content);
     }
+    
+    DtwStringArray_free(files);
 }
